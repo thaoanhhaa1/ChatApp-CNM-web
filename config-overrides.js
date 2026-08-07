52279b3066b1351d163e095565f955f34c17d4ea
Dev Manager <thaoanhhaa1@gmail.com>
2026-08-08 01:07:05 +0700

    fix: suppress Invalid URL warnings in webpack configuration

--- config-overrides.js
+++ config-overrides.js
@@ -3,4 +3,11 @@ const { override, useBabelRc } = require('customize-cra');
 module.exports = override(
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useBabelRc(),
+    (config) => {
+        config.ignoreWarnings = [
+            ...(config.ignoreWarnings || []),
+            (warning) => warning.message && warning.message.includes('Invalid URL'),
+        ];
+        return config;
+    }
 );
